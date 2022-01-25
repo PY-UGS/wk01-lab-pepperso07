44ce185542ce63c95ba78654560efa18e619d0b4
pepperso07 <97490198+pepperso07@users.noreply.github.com>
2022-01-25 10:46:18 +0800

    Add files via upload

---
+++ Q2.java
@@ -0,0 +1,26 @@
+package Wk1;
+
+public class Q2 {
+public static void main(String[] args) {
+    String currency = "CSC1009";
+switch(currency)
+{
+case "CSC1010":
+System.out.println("Computer networks");
+break;
+case "CSC1008":
+System.out.println("Data structures and algorithm");
+break;
+case "CSC1007":
+System.out.println("Operating systems");
+break;
+case "CSC1009":
+System.out.println("Object oriented programming");
+break;
+default:
+System.out.println("Unknown course");
+break;
+}
+System.out.println("After switch");
+}
+}
---
+++ Q3.java
@@ -0,0 +1,12 @@
+package Wk1;
+
+public class Q3 {
+public static void main(String[] args) {
+	 
+	for (int x=102; x>=66; x=x-1)
+    {
+      System.out.print("Value of x is:" + x);
+        System.out.print("\n");
+    }
+}
+}
